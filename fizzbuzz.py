a3046827e42f300101313432e26a24be474a823a
Wara Wangtrakoon <babythor@PRONTOLAP0022.local>
2014-11-15 20:43:29 +0700

    create fizzbuzz problem and example for output for this problem

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+'''
+The problem is
+
+If input number divisible by 3 will return string "Fizz"
+If input number divisible by 5 will return string "Buzz"
+If input number divisible by 3 and 5 will return string "FizzBuzz"
+If input number can not divisible by 3 and 5 will return string of this number
+
+Example
+input(1) -> "1"
+input(3) -> "Fizz"
+input(5) -> "Buzz"
+input(6) -> "Fizz"
+input(10) -> "Buzz"
+input(15) -> "FizzBuzz"
+input(30) -> "FizzBuzz"
+'''
